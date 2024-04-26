1d94438aa701d98af5cc3b9cd12584e0cfc4d7b9
vaclav <vaclav.krivka07@gmail.com>
2024-04-26 21:26:11 +0200

    Adding morse code symbols and creating Translator class

---
+++ app/src/main/java/com/example/morsecode/MorseCodeSymbols.java
@@ -0,0 +1,4 @@
+package com.example.morsecode;
+public enum MorseCodeSymbols {
+    DOT, LINE, SEPARATOR, SPACE, PERIOD
+}
---
+++ app/src/main/java/com/example/morsecode/Translator.java
@@ -0,0 +1,14 @@
+package com.example.morsecode;
+
+import java.util.ArrayList;
+
+//This class is used to translate letters into morse code
+public class Translator {
+
+    public ArrayList<MorseCodeSymbols> translate(String input) {
+        ArrayList<MorseCodeSymbols> result = new ArrayList<>();
+        //TBC
+        return result;
+    }
+
+}

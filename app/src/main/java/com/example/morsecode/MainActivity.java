0e97fcd45d6dbf788b26b371608cb780063f3fe3
vaclav <vaclav.krivka07@gmail.com>
2024-05-04 22:57:09 +0200

    created other buttons

--- app/src/main/java/com/example/morsecode/MainActivity.java
+++ app/src/main/java/com/example/morsecode/MainActivity.java
@@ -2,12 +2,101 @@ package com.example.morsecode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button buttonSlow;
+    private Button buttonMedium;
+    private Button buttonFast;
+
+    private TextView speed;
+    private boolean hasCameraFlash = false;
+    private boolean flashOn = false;
+    private int speedMultiplier;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        buttonSlow = findViewById(R.id.buttonSlow);
+        buttonMedium = findViewById(R.id.buttonMedium);
+        buttonFast = findViewById(R.id.buttonFast);
+
+        hasCameraFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        speed = findViewById(R.id.speedIndicator);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            //borrowed from https://github.com/msindev/FlashLight
+            @Override
+            public void onClick(View v) {
+                if(hasCameraFlash){
+                    if(flashOn){
+                        flashOn = false;
+                        flashLightOff();
+                    }
+                    else{
+                        flashOn = true;
+                        flashLightOn();
+                    }
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "No flash available on your device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonSlow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speed.setText("Medium");
+
+            }
+        });
     }
 
+
+    //borrowed from https://github.com/msindev/FlashLight
+    private void flashLightOn(){
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            assert cameraManager != null;
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+            Toast.makeText(MainActivity.this, "FlashLight is ON", Toast.LENGTH_SHORT).show();
+        }
+        catch(CameraAccessException e){
+            Log.e("Camera Problem", "Cannot turn on camera flashlight");
+        }
+    }
+
+    //borrowed from https://github.com/msindev/FlashLight
+    private void flashLightOff(){
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            assert cameraManager != null;
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+            Toast.makeText(MainActivity.this, "FlashLight is OFF", Toast.LENGTH_SHORT).show();
+        }
+        catch(CameraAccessException e){
+            Log.e("Camera Problem", "Cannot turn off camera flashlight");
+        }
+
+
+    }
 }
